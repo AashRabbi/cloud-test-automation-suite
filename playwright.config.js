82a241f5788ab924bda742a3e980706002addf51
aashrabbi <aash.rabbi@gmail.com>
2025-05-19 17:00:00 -0500

    Update Playwright configuration - 2025-05-19

--- playwright.config.js
+++ playwright.config.js
@@ -54,3 +54,4 @@ Updated configuration for 2025-05-09
 Updated configuration for 2025-05-13
 Updated configuration for 2025-05-14
 Updated configuration for 2025-05-15
+Updated configuration for 2025-05-19
